f9586a709b6c0fe183d1dd6c12756d72dc4ebf8d
Harjot Sandhu <hsandhu4@masonlive.gmu.edu>
2017-04-11 17:36:51 -0400

    modified host to connect to node on aws

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,5 +2,5 @@ import RESTAdapter from 'ember-data/adapters/rest';
 
 export default RESTAdapter.extend({
 //  namespace: 'v1',
-  host: 'http://localhost:3000',
+  host: 'http://ec2-34-201-7-46.compute-1.amazonaws.com:3000',
 });
